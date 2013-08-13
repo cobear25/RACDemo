d5de46a6d5d1390065f6bb9bd22bb8d5771a6c1e
Cody Mace <codymace@gmail.com>
2013-08-13 11:01:33 -0600

    Initial Commit

---
+++ RACDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RACDemo
+//
+//  Created by Cody Mace on 8/13/13.
+//  Copyright (c) 2013 Cody Mace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RACDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RACDemo
+//
+//  Created by Cody Mace on 8/13/13.
+//  Copyright (c) 2013 Cody Mace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
